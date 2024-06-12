a1b37904cc462de3ae68b31757ee8649a22a6d25
Fel <felgonzalez951@gmail.com>
2024-06-12 13:47:27 -0600

    added '/dashboard' route to homepageRoutes, subject to change

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -12,20 +12,25 @@ router.get('/', async (req, res) => {
     }
 });
 
-router.get('/dashboard', async (req, res) => {
+router.get('/dashboard', withAuth, async (req, res) => {
     try {
-        const userData = await User.findAll({
-            include: [
-                {
-                    model: Budget,
-                    attributes: ['income'],
-                },
-            ],
+        const userData = await User.findByPk(req.session.user_id, {
+            attributes: { exclude: ['password'] },
+            include: [{ model: Budget }],
         });
 
-        //FILL IN LATER
+        const user = userData.get({ plain: true });
+
+        res.render('dashboard', {
+            ...user,
+            logged_in: true
+        });
     } catch (err) {
         res.status(500).json(err);
     }
 });
 
+// ! There's no login route since the form is ingrained on the homepage
+// ! Please let me know if that needs to be changed
+
+module.exports = router
