feb604fe8db71058825fbd5725e160805300d5fe
Fel <felgonzalez951@gmail.com>
2024-06-15 16:31:53 -0600

    removed massive commented out code

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -106,33 +106,6 @@ router.get('/category/:id', withAuth, async (req, res) => {
         res.status(500).json({ error: 'Internal server error' });
     }
 });
-  
-//   router.get('/category/:id', withAuth, async (req, res) => {
-//     try {
-//         // Fetch category data with associated transactions
-//         const categoryData = await Category.findByPk(req.params.id, {
-//             include: {
-//                 model: Transaction
-//             }
-//         });
-
-//         if (!categoryData) {
-//             return res.status(404).json({ message: 'Category not found' });
-//         }
-
-//         const category = categoryData.toJSON(); // Convert to plain JSON object
-
-//         // Render category.handlebars with category data
-//         res.render('category', {
-//             category,
-//             logged_in: true
-//         });
-//     } catch (err) {
-//         console.error('Error fetching category data:', err);
-//         res.status(500).json({ error: 'Internal server error' });
-//     }
-// });
-
 
 // ! There's no login route since the form is ingrained on the homepage
 // ! Please let me know if that needs to be changed
